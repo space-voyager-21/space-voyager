2c5c6b1986eb02fda09d40445d1c56775a63e00f
Stars Tracker <techixspotify@gmail.com>
2021-10-05 12:52:44 +0530

    refactor(Satellite): Ready to review

--- src/components/satellite/Satellite.js
+++ src/components/satellite/Satellite.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import styles from "./Satellite.module.css";
-
+import LoadTime from "../LoadTimePage";
+import Error from "../Error";
+import { allCountriesList } from "countries-code";
 export default class Satellite extends Component {
   constructor(props) {
     super(props);
@@ -20,11 +22,13 @@ export default class Satellite extends Component {
        *}
        */
       data: null,
+      error: false,
     };
     this.state.handleSubmit = this.handleSubmit.bind(this);
   }
   handleSubmit = async (e) => {
     e.preventDefault();
+    this.setState({ error: false, data: null });
     const value = e.target.children[0].value;
     if (!value?.trim()) return;
     e.target.children[0].value = "";
@@ -34,11 +38,10 @@ export default class Satellite extends Component {
       );
 
       const data = await response.json();
-      
+
       this.setState({ data: data[0].Sattelite });
     } catch (e) {
-      console.log(e);
-      console.log("error");
+      this.setState({ error: true });
     }
   };
 
@@ -55,12 +58,50 @@ export default class Satellite extends Component {
             <input className="submit" type="submit" value="Submit" />
           </form>
         </div>
-        {this.state.data && (
-          <div className={styles["satellite-info"]}>
-            <h3>Name : {this.state.data.moderateInfo.name}</h3>
-            <h3>Country : {this.state.data.moderateInfo.country_of_origin}</h3>
-          </div>
-        )}
+        {(this.state.error && <Error />) ||
+          (this.state.data && (
+            <div className={styles["satellite-info"]}>
+              <h1>
+                {this.state.data.moderateInfo.name}{" "}
+                {this.state.data.moderateInfo.country_of_origin && (
+                  <img
+                    alt=""
+                    src={
+                      "https://www.countryflags.io/" +
+                      allCountriesList()
+                        .find(
+                          (x) =>
+                            x.country_name_en.toLowerCase() ===
+                            this.state.data.moderateInfo.country_of_origin.toLowerCase()
+                        )
+                        ?.alpha2?.toLowerCase() +
+                      "/flat/64.png"
+                    }
+                  ></img>
+                )}
+              </h1>
+              <hr></hr>
+              <h3>Name : {this.state.data.moderateInfo.name}</h3>
+              <h3>
+                Country :{" "}
+                {this.state.data.moderateInfo.country_of_origin ?? "N/A"}
+              </h3>
+              <h3>
+                Date of Launch : {this.state.data.moderateInfo.date_of_launch}
+              </h3>
+              <h3>Type : {this.state.data.moderateInfo.type ?? "N/A"}</h3>
+              <h3>
+                Coverage : {this.state.data.moderateInfo.coverage ?? "N/A"}
+              </h3>
+              <h3>
+                Operator : {this.state.data.moderateInfo.operator ?? "N/A"}
+              </h3>
+              <h3>
+                Sattelite Bus :{" "}
+                {this.state.data.moderateInfo.sattelite_bus ?? "N/A"}
+              </h3>
+            </div>
+          )) || <LoadTime />}
       </div>
     );
   }
