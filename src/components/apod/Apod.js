0adb314de9fc5961cb1eb345181c085c00007a05
ramdevcm <ramdevcm@gmail.com>
2021-10-10 13:48:02 +0530

    Added in-built media player

--- src/components/apod/Apod.js
+++ src/components/apod/Apod.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import ReactPlayer from 'react-player/lazy';
 import styles from "./Apod.module.css";
 import Constants from "./../Constant";
 
@@ -14,23 +15,26 @@ export default function Apod() {
       setApod(data);
     })();
   }, []);
+  // console.log(apod.url)
   return (
     <div className={styles["apod"]}>
       {apod && (
         <div>
-            <div className={styles["apod__main__header"]}>🌌Astronomy Pic of the day!🌌</div>
+          <div className={styles["apod__main__header"]}>🌌Astronomy Pic of the day!🌌</div>
           <div className={styles["apod__header"]}>{apod.title}</div>
           <div className={styles["apod__description"]}>{apod.explanation}</div>
+          <div className='player-wrapper'>
+            <ReactPlayer
+              className='react-player'
+              width='100%'
+              url={apod.url}
+              playing="true"
+              controls="true"
+            />
+          </div>
         </div>
       )}
-      <div
-        className={styles["apod__image"]}
-        onClick={() => apod && window.open(apod.url)}
-      >
-        {(apod && <img src={apod.url} alt={apod.title} />) || (
-          <div class="lds-hourglass"></div>
-        )}
-      </div>
+
     </div>
   );
 }
