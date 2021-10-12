54d1fe2f5e55ad9b59676cd193ee12bee9197377
Stars Tracker <techixspotify@gmail.com>
2021-10-12 10:24:29 +0530

    fix(APOD): fix appearing of media player when image was returned from the api

--- src/components/apod/Apod.js
+++ src/components/apod/Apod.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import ReactPlayer from 'react-player/lazy';
+import ReactPlayer from "react-player/lazy";
 import styles from "./Apod.module.css";
 import Constants from "./../Constant";
 
@@ -20,21 +20,31 @@ export default function Apod() {
     <div className={styles["apod"]}>
       {apod && (
         <div>
-          <div className={styles["apod__main__header"]}>🌌Astronomy Pic of the day!🌌</div>
+          <div className={styles["apod__main__header"]}>
+            🌌Astronomy Pic of the day!🌌
+          </div>
           <div className={styles["apod__header"]}>{apod.title}</div>
           <div className={styles["apod__description"]}>{apod.explanation}</div>
-          <div className='player-wrapper'>
-            <ReactPlayer
-              className='react-player'
-              width='100%'
-              url={apod.url}
-              playing="true"
-              controls="true"
-            />
-          </div>
+          {(apod.url?.endsWith(".jpg") && (
+            <div
+              className={styles["apod__image"]}
+              onClick={() => apod && window.open(apod.url)}
+            >
+              <img src={apod.url} alt={apod.title} />
+            </div>
+          )) || (
+            <div className="player-wrapper">
+              <ReactPlayer
+                className="react-player"
+                width="100%"
+                url={apod.url}
+                playing="true"
+                controls="true"
+              />
+            </div>
+          )}
         </div>
       )}
-
     </div>
   );
 }
