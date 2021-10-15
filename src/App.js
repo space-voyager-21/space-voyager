8204a616183375801c604a0b156af5a81b979558
Tyler Morales <moratyle@gmail.com>
2021-10-15 13:25:52 -0500

    ✨  Make planets flex and responsive

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React, { useState, useEffect } from "react";
-import "./components/index.css";
-import img1 from "./components/img1.png";
-import "animate.css";
-import "./components/error.css";
-import "./components/LoadTimePage.css";
-import LoadTime from "./components/LoadTimePage";
-import axiosInstance from "./axios";
-import ErrorComponent from "./components/Error";
-import Constants from "./components/Constant";
-import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
-import Particles from "react-particles-js";
-import IssMap from "./components/iss-map/IssMap";
+import React, { useState, useEffect } from 'react'
+import './components/index.css'
+import img1 from './components/img1.png'
+import 'animate.css'
+import './components/error.css'
+import './components/LoadTimePage.css'
+import LoadTime from './components/LoadTimePage'
+import axiosInstance from './axios'
+import ErrorComponent from './components/Error'
+import Constants from './components/Constant'
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
+import Particles from 'react-particles-js'
+import IssMap from './components/iss-map/IssMap'
+
+import './PlanetStyles.css'
 
 function App() {
-  const [data, setData] = useState([]);
-  const [path, setPath] = useState([]);
-  const [query, setQuery] = useState("");
-  const [error, setError] = useState(false);
-  const [load, setLoad] = useState(false);
-  const [querystatus, setquerystatus] = useState(false);
+  const [data, setData] = useState([])
+  const [path, setPath] = useState([])
+  const [query, setQuery] = useState('')
+  const [error, setError] = useState(false)
+  const [load, setLoad] = useState(false)
+  const [querystatus, setquerystatus] = useState(false)
   // const fetchApidata = async () => {
   //   const api = await fetch('https://api.le-systeme-solaire.net/rest/bodies/');
   //   const apiData = await api.json();
@@ -28,41 +30,41 @@ function App() {
   // }
 
   useEffect(() => {
-    setLoad(true);
-    setError(false);
-    setquerystatus(false);
-  }, []);
+    setLoad(true)
+    setError(false)
+    setquerystatus(false)
+  }, [])
 
   const storeData = async (query) => {
     try {
-      setError(false);
-      const result = await axiosInstance.get(query);
-      setData(result.data);
+      setError(false)
+      const result = await axiosInstance.get(query)
+      setData(result.data)
       try {
-        const imgPath = require("../assets/" +
+        const imgPath = require('../assets/' +
           result.data.englishName +
-          ".png").default;
-        setPath(imgPath);
+          '.png').default
+        setPath(imgPath)
       } catch {
-        setPath(img1);
+        setPath(img1)
       }
-      setquerystatus(true);
-      setQuery("");
+      setquerystatus(true)
+      setQuery('')
     } catch (error) {
-      setData("");
-      setError(true);
-      setquerystatus(true);
+      setData('')
+      setError(true)
+      setquerystatus(true)
     }
   }
 
   const fetchData = (event) => {
-    event.preventDefault();
-    storeData(query);
-  };
+    event.preventDefault()
+    storeData(query)
+  }
 
-  const planetClick = (event,planetName) =>{
-    event.preventDefault();
-    storeData(planetName);
+  const planetClick = (event, planetName) => {
+    event.preventDefault()
+    storeData(planetName)
   }
 
   return (
@@ -95,18 +97,117 @@ function App() {
             </form>
           </div>
           <div>
-          <div className="bar">
-                {/* <img alt="Earth" title="Earth" src={Constants.earthPath} onClick={fetchData}/> */}
-                <img alt="Mercury" title="Mercury" src={Constants.mercuryPath} onClick={(event) => {planetClick(event,"Mercury")}}/>
-                <img alt="Venus" value="Venus" title="Venus" src={Constants.venusPath} onClick={(event) => {planetClick(event,"Venus")}}/>
-                <img alt="Earth" value="Earth" title="Earth" src={Constants.earthPath} onClick={(event) => {planetClick(event,"Earth")}}/>
-                <img alt="Mars" value="Mars" title="Mars" src={Constants.marsPath} onClick={(event) => {planetClick(event,"Mars")}}/>
-                <img alt="Jupiter" value="Jupiter" title="Jupiter" src={Constants.jupiterPath} onClick={(event) => {planetClick(event,"Jupiter")}}/>
-                <img alt="Saturn" value="Saturn" title="Saturn" src={Constants.saturnPath} onClick={(event) => {planetClick(event,"Saturn")}}/>
-                <img alt="Uranus" value="Uranus" title="Uranus" src={Constants.uranusPath} onClick={(event) => {planetClick(event,"Uranus")}}/>
-                <img alt="Neptune" value="Neptune" title="Neptune" src={Constants.neptunePath} onClick={(event) => {planetClick(event,"Neptune")}}/>
-                <img alt="Pluto" value="Pluto" title="Pluto" src={Constants.plutoPath} onClick={(event) => {planetClick(event,"Pluto")}}/>
-              </div>
+            <div className="planets">
+              {/* <img alt="Earth" title="Earth" src={Constants.earthPath} onClick={fetchData}/> */}
+              <button
+                onClick={(event) => {
+                  planetClick(event, 'Mercury')
+                }}
+              >
+                <img
+                  className="planet"
+                  alt="Mercury"
+                  title="Mercury"
+                  src={Constants.mercuryPath}
+                />
+              </button>
+              <button
+                onClick={(event) => {
+                  planetClick(event, 'Venus')
+                }}
+              >
+                <img
+                  className="planet"
+                  alt="Venus"
+                  title="Venus"
+                  src={Constants.venusPath}
+                />
+              </button>
+              <button
+                onClick={(event) => {
+                  planetClick(event, 'Earth')
+                }}
+              >
+                <img
+                  className="planet"
+                  alt="Earth"
+                  title="Earth"
+                  src={Constants.earthPath}
+                />
+              </button>
+              <button
+                onClick={(event) => {
+                  planetClick(event, 'Mars')
+                }}
+              >
+                <img
+                  className="planet"
+                  alt="Mars"
+                  title="Mars"
+                  src={Constants.marsPath}
+                />
+              </button>
+              <button
+                onClick={(event) => {
+                  planetClick(event, 'Jupiter')
+                }}
+              >
+                <img
+                  className="planet"
+                  alt="Jupiter"
+                  title="Jupiter"
+                  src={Constants.jupiterPath}
+                />
+              </button>
+              <button
+                onClick={(event) => {
+                  planetClick(event, 'Saturn')
+                }}
+              >
+                <img
+                  className="planet"
+                  alt="Saturn"
+                  title="Saturn"
+                  src={Constants.saturnPath}
+                />
+              </button>
+              <button
+                onClick={(event) => {
+                  planetClick(event, 'Uranus')
+                }}
+              >
+                <img
+                  className="planet"
+                  alt="Uranus"
+                  title="Uranus"
+                  src={Constants.uranusPath}
+                />
+              </button>
+              <button
+                onClick={(event) => {
+                  planetClick(event, 'Neptune')
+                }}
+              >
+                <img
+                  className="planet"
+                  alt="Neptune"
+                  title="Neptune"
+                  src={Constants.neptunePath}
+                />
+              </button>
+              <button
+                onClick={(event) => {
+                  planetClick(event, 'Pluto')
+                }}
+              >
+                <img
+                  className="planet"
+                  alt="Pluto"
+                  title="Pluto"
+                  src={Constants.plutoPath}
+                />
+              </button>
+            </div>
           </div>
           <Particles
             data-testid={Constants.particleName}
@@ -114,7 +215,7 @@ function App() {
             params={Constants.particleParam}
           />
           {error && <ErrorComponent type="planet" />}
-          {typeof data.name != "undefined" ? (
+          {typeof data.name != 'undefined' ? (
             <>
               <div className="detailbox">
                 <h2> {data.englishName} </h2>
@@ -125,61 +226,61 @@ function App() {
 
               <div className="deepbox">
                 <h3>
-                  {" "}
-                  Mass of {data.englishName} : {data.mass.massValue}{" "}
-                  <span>&#215;</span> 10<sup>{data.mass.massExponent}</sup> kg{" "}
+                  {' '}
+                  Mass of {data.englishName} : {data.mass.massValue}{' '}
+                  <span>&#215;</span> 10<sup>{data.mass.massExponent}</sup> kg{' '}
                 </h3>
                 <h3>
-                  {" "}
-                  Mean Temperature of {data.englishName} : {data.avgTemp} K{" "}
+                  {' '}
+                  Mean Temperature of {data.englishName} : {data.avgTemp} K{' '}
                 </h3>
                 <h3>
-                  {" "}
-                  Mean radius of {data.englishName} : {data.meanRadius} km{" "}
+                  {' '}
+                  Mean radius of {data.englishName} : {data.meanRadius} km{' '}
                 </h3>
                 <h3>
-                  {" "}
-                  Equitorial Radius of {data.englishName} : {data.equaRadius} km{" "}
+                  {' '}
+                  Equitorial Radius of {data.englishName} : {data.equaRadius} km{' '}
                 </h3>
                 <h3>
-                  {" "}
-                  Polar radius of {data.englishName} : {data.polarRadius} km{" "}
+                  {' '}
+                  Polar radius of {data.englishName} : {data.polarRadius} km{' '}
                 </h3>
                 <h3>
-                  {" "}
+                  {' '}
                   Surface Gravity of {data.englishName} : {data.gravity} m/s
-                  <sup>2</sup>{" "}
+                  <sup>2</sup>{' '}
                 </h3>
                 <h3>
-                  {" "}
-                  Escape Velocity of {data.englishName} :{" "}
-                  {Math.floor(data.escape / 1000).toFixed(2)} km/s{" "}
+                  {' '}
+                  Escape Velocity of {data.englishName} :{' '}
+                  {Math.floor(data.escape / 1000).toFixed(2)} km/s{' '}
                 </h3>
                 <h3>
-                  {" "}
-                  Axial tilt of {data.englishName} : {data.axialTilt} °{" "}
+                  {' '}
+                  Axial tilt of {data.englishName} : {data.axialTilt} °{' '}
                 </h3>
                 <h3>
-                  {" "}
-                  Rotation period of {data.englishName}: {data.sideralRotation}{" "}
-                  hrs{" "}
+                  {' '}
+                  Rotation period of {data.englishName}: {data.sideralRotation}{' '}
+                  hrs{' '}
                 </h3>
                 <h3>
-                  {" "}
-                  Revolution period of {data.englishName}: {data.sideralOrbit}{" "}
-                  days{" "}
+                  {' '}
+                  Revolution period of {data.englishName}: {data.sideralOrbit}{' '}
+                  days{' '}
                 </h3>
                 <h3>
-                  {" "}
-                  Density of {data.englishName}: {data.density} g/cm<sup>3</sup>{" "}
+                  {' '}
+                  Density of {data.englishName}: {data.density} g/cm<sup>3</sup>{' '}
                 </h3>
                 <h3>
-                  {" "}
-                  Aphelion of {data.englishName}: {data.aphelion} km{" "}
+                  {' '}
+                  Aphelion of {data.englishName}: {data.aphelion} km{' '}
                 </h3>
                 <h3>
-                  {" "}
-                  Perihelion period of {data.englishName}: {data.perihelion} km{" "}
+                  {' '}
+                  Perihelion period of {data.englishName}: {data.perihelion} km{' '}
                 </h3>
                 {/* <h3> Moons of {data.englishName}: {data.moons.map((elem, index) =>{
             return (<span> {elem.moon}, </span>) 
@@ -188,7 +289,7 @@ function App() {
               </div>
             </>
           ) : (
-            ""
+            ''
           )}
           {error === false && load && querystatus === false && <LoadTime />}
           {/* <Link to="/iss-location" className="map-button" >View ISS Location</Link> */}
@@ -202,6 +303,6 @@ function App() {
         </h2>
       </div> */}
     </BrowserRouter>
-  );
+  )
 }
-export default App;
+export default App
